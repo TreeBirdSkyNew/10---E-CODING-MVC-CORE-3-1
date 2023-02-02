29778dc399b867921f678023e1bfde1190408736
PEREIRA <filousofinho@outlook.fr>
2023-02-02 17:45:04 +0100

    test commit

---
+++ __WEB_API__TemplateFonctionnel-WebApi-Tests/_TemplateFonctionnel/UnitTest/TemplateFonctionnelControllerUnitTest.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using E_CODING_MVC_NET6_0.Models;
+using E_CODING_Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace __WEB_API__TemplateFonctionnel_WebApi_Tests._TemplateFonctionnel.UnitTest
+{
+    public class TemplateFonctionnelControllerUnitTest
+    {
+        private readonly Mock<ITemplateFonctionnelService> _mockItemplateFonctionnelService;
+        private readonly Mock<IMapper> _mockMapper;
+
+        public TemplateFonctionnelControllerUnitTest()
+        {
+            _mockMapper = new Mock<IMapper>();
+            _mockItemplateFonctionnelService = new Mock<ITemplateFonctionnelService>();
+        }
+
+        public TemplateFonctionnelControllerUnitTest Instance()
+        {
+            return new TemplateFonctionnelControllerUnitTest();
+        }
+
+        [Fact]
+        public void UnitTestTemplateFonctionnelIndex()
+        {
+            List<TemplateFonctionnelVM> listeTemplateFonctionnelVM = new List<TemplateFonctionnelVM>();
+            listeTemplateFonctionnelVM.Add(
+                new TemplateFonctionnelVM()
+                {
+                    TemplateFonctionnelId=1,
+                    TemplateFonctionnelName= "TemplateFonctionnelName1",
+                    TemplateFonctionnelTitle = "TemplateFonctionnelTitle1",
+                    TemplateFonctionnelDescription = "TemplateFonctionnelDescription1",
+                    TemplateFonctionnelContent = "TemplateFonctionnelContent1",
+                    TemplateFonctionnelEFVersion = "TemplateFonctionnelEFVersion1",
+                    TemplateProjectId = 1,
+                    TemplateFonctionnelEntity = new List<TemplateFonctionnelEntityVM>()
+                    {
+                        
+                    }
+                }
+            );
+        }
+    }
+}
